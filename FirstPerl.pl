464156dfd135da772e9955540e87f5df4d6ac9ab
eyeofsan <36434472+eyeofsan@users.noreply.github.com>
2018-02-13 12:25:41 +0530

    Create FirstPerl.pl

---
+++ FirstPerl.pl
@@ -0,0 +1,9 @@
+print "Hello Universe \n" ; #it has become a practise :P
+
+my ($name,$age) = ("Santhosh","22");
+
+print qq{my name is $name and my age is $age } ; 
+
+$my_life = "you" ;
+
+print qq{\n \n \n  I Love $my_life \n \n \n } ;  #Love is in the air 
