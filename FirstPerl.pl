ad466f32e103ccd0deafcd27dc94dc608e01c8d8
eyeofsan <36434472+eyeofsan@users.noreply.github.com>
2023-03-20 21:50:40 +0530

    Delete FirstPerl.pl
    deleted

--- FirstPerl.pl
+++
@@ -1,9 +0,0 @@
-print "Hello Universe \n" ; #it has become a practise :P
-
-my ($name,$age) = ("Santhosh","22");
-
-print qq{my name is $name and my age is $age } ; 
-
-$my_life = "you" ;
-
-print qq{\n \n \n  I Love $my_life \n \n \n } ;  #Love is in the air 
